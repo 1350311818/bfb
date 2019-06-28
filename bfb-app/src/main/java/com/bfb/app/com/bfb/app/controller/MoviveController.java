1339f0f8e314667afabd1393fbeca23c41295f71
何景浩 <1350311818@qq.com>
2019-06-29 02:22:42 +0800

    使用zk作为注册中心，日后方便使用kafa

--- bfb-app/src/main/java/com/bfb/app/com/bfb/app/controller/MoviveController.java
+++ bfb-app/src/main/java/com/bfb/app/com/bfb/app/controller/MoviveController.java
@@ -1,46 +1,40 @@
 package com.bfb.app.com.bfb.app.controller;
 
-import com.bfb.app.com.bfb.app.lock.AquiredLockWorker;
-import com.bfb.app.com.bfb.app.lock.DistributedLocker;
-import com.bfb.app.com.bfb.app.lock.RedisLock;
-import com.bfb.app.com.bfb.app.lock.RedisLocker;
-import org.redisson.api.RLock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MoviveController {
-    @Autowired
-    private RedisLocker RedisLocker;
-    @Autowired
-    private RedisLock redisLock;
+//    @Autowired
+//    private RedisLocker RedisLocker;
+//    @Autowired
+//    private RedisLock redisLock;
     @GetMapping("/user/{id}")
     public String findText(){
-       RLock rLock= redisLock.getLock("test");
-        try {
-            RedisLocker.lock(rLock,new AquiredLockWorker<Object>(){
-
-                @Override
-                public Object invokeAfterLockAquire() throws Exception {
-                    System.out.println("执行方法！");
-
-                    return null;
-                }
-
-                @Override
-                public Object invokeAfterLockFail()  {
-                    System.out.println("执锁失败");
-                    return null;
-                }
-            });
-        } catch (Exception e) {
-            return  "fail";
-
-
-        }finally {
-            rLock.unlock();
-        }
+//       RLock rLock= redisLock.getLock("test");
+//        try {
+//            RedisLocker.lock(rLock,new AquiredLockWorker<Object>(){
+//
+//                @Override
+//                public Object invokeAfterLockAquire() throws Exception {
+//                    System.out.println("执行方法！");
+//
+//                    return null;
+//                }
+//
+//                @Override
+//                public Object invokeAfterLockFail()  {
+//                    System.out.println("执锁失败");
+//                    return null;
+//                }
+//            });
+//        } catch (Exception e) {
+//            return  "fail";
+//
+//
+//        }finally {
+//            rLock.unlock();
+//        }
         return  "test";
     }
 }
